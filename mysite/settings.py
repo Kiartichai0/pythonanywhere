8cf4f42d2a841fe2d1289a941df9dba5fc7fe788
nut <kiartichai.su.61@ubu.ac.th>
2019-10-01 15:54:17 +0700

    hell

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=7-j+ch$3yzyj0=d$2-lp%c#+k$#fitw6mm=dq5+g)yy@)0b@&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'kiartichai.pythonanywhere.com']
+ALLOWED_HOSTS = [u'kiartichai.pythonanywhere.com',u'127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views as blogview
 
 urlpatterns = [
+    path('', blogview.index),
     path('admin/', admin.site.urls),
 ]
